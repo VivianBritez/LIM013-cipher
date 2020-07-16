1bd383e0cbf95f0135656480a74057da02aef07b
VivianBritez <vivianbritez91@gmail.com>
2020-07-16 07:01:26 -0500

    los archivos fueron creados

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,23 @@
-const cipher = {
+alert("hola mundo");
+import mensaje from './index.js';
+const cipher = (userMessage, n) => {
+  let result= "", codigo;
+  for(let i= 0; i < userMessage.length; i++){
+    //mayuscula 65 al 91
+    if(userMessage.charCodeAt(i)>= 65 && userMessage.charCodeAt(i)<=90){
+      codigo= (((userMessage.charCodeAt(i)-65)+n)%26)+65;  
+    }
+    // minuscula 97-202
+    else if( userMessage.charCodeAt(i)>=97 && userMessage.charCodeAt(i)<=122){
+      codigo= (((userMessage.charCodeAt(i)-97)+n)%26)+97;
+    }
+    // 32 es espacio 
+    else if(userMessage.charCodeAt(i)===32){
+      code=32;
+    }
+    result+= userMessage.fromCharCode(codigo);
+  }
+  return result;
   // ...
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import cipher from './cipher.js';
+alert("soy index");
 
+import cipher from './cipher.js';
 console.log(cipher);
+let mensaje= document.querySelector("#mensaje");
+if(mensaje){
+    mensaje.addEventListener("submit", function() {
+        // saca el valor de mensaje
+    let userMessage= document.getElementById("userMensaje1")[0].value;
+        cipher(userMessage, 22);
+});
+};
+console.log(cipher);
+
+export default mensaje;
