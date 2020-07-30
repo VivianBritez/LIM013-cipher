fd12d26b9436b0d7a8136d39e93c437c624a4c50
VivianBritez <vivianbritez91@gmail.com>
2020-07-30 17:48:09 -0500

    test realizado

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -21,7 +21,7 @@ describe('cipher', () => {
     });
 
     it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
-      expect(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ',33 )).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
+      expect(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33)).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
 
     // Hacker edition
@@ -34,9 +34,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-     it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-      expect(cipher.encode('abcdefghijklmnopqrstuvwxyz',33 )).toBe('hijklmnopqrstuvwxyzabcdefg');
-     });
+    it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+      expect(cipher.encode('abcdefghijklmnopqrstuvwxyz', 33)).toBe('hijklmnopqrstuvwxyzabcdefg');
+    });
 
     // Hacker edition
     //
@@ -67,7 +67,7 @@ describe('cipher', () => {
     });
 
     it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
-      expect(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG',33)).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+      expect(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG', 33)).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
 
     //
@@ -81,8 +81,8 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-     it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-      expect(cipher.decode('hijklmnopqrstuvwxyzabcdefg',33 )).toBe('abcdefghijklmnopqrstuvwxyz');
+    it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+      expect(cipher.decode('hijklmnopqrstuvwxyzabcdefg', 33)).toBe('abcdefghijklmnopqrstuvwxyz');
     });
 
     // Hacker edition
